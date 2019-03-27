da3e5872f4f3be873746ac8b45748c873d1d66b8
kenmhaggerty <kenmhaggerty@protonmail.com>
2019-03-27 07:59:04 -0500

    Create wheel_of_fortune.js with instructions

---
+++ wheel_of_fortune.js
@@ -0,0 +1,18 @@
+// wheel_of_fortune.js
+// CoderGirl WebDev (Winter 2019) — LaunchCode
+
+// Set a secret word!
+
+// Ask player if they would like to guess a letter or guess the word
+
+	// If guessing a letter...
+
+		// If the word contains the letter, print back the word with all instances of that letter revealed.
+
+		// If the word does not contain the letter, print back "Sorry, no <letter>!" but with <letter> replaced by the player's guess.
+
+	// If guessing the word...
+
+		// If they are correct, congratulate the player!
+
+		// If they are incorrect, print back "Nope, keep trying!"
