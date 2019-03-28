1e4d0403695f184892b32160e8ee00ed991197d5
kenmhaggerty <kenmhaggerty@protonmail.com>
2019-03-27 21:30:45 -0500

    Clarifies instructions in wheel_of_fortune.js

--- wheel_of_fortune.js
+++ wheel_of_fortune.js
@@ -1,18 +1,45 @@
 // wheel_of_fortune.js
 // CoderGirl WebDev (Winter 2019) — LaunchCode
 
+// VARIABLES /////////////////////////////////////////////////////////
+
+// In a normal application you would ask for user input.
+// In this exercise we will set the variables ourselves.
+
 // Set a secret word!
 
-// Ask player if they would like to guess a letter or guess the word
+var word = 'ANSWER'; // ALL CAPS
 
-	// If guessing a letter...
+// Ask player if they would like to guess a letter or guess the
+// solution.
 
-		// If the word contains the letter, print back the word with all instances of that letter revealed.
+var isGuessingLetter = true; // set to false to guess solution
 
-		// If the word does not contain the letter, print back "Sorry, no <letter>!" but with <letter> replaced by the player's guess.
+// If they are guessing a letter, set the letter that they are
+// guessing.
 
-	// If guessing the word...
+var letter = 'A'; // UPPERCASE
 
-		// If they are correct, congratulate the player!
+// SCRIPT ////////////////////////////////////////////////////////////
 
-		// If they are incorrect, print back "Nope, keep trying!"
+// If guessing a letter...
+
+	// If the word contains the letter...
+
+		// Print back the word with all instances of that letter revealed.
+
+	// If the word does not contain the letter...
+
+		// Print back "Sorry, no <letter>!" but with <letter> replaced by the player's guess.
+
+// If guessing the word...
+
+	// If they are correct...
+
+		// Print "Congratulations! You won Wheel Of Fortune!"
+
+	// If they are incorrect...
+
+		// Print back "Nope, keep trying!"
+
+// End of script!
